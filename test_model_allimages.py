75c312c87fff3ddf633db082f63c26c44ed68ccb
mabelle2024 <mabelle.george@btech.christuniversity.in>
2024-11-30 00:25:53 +0900

    test_model_allimages.py

--- test_model_allimages.py
+++ test_model_allimages.py
@@ -1,6 +1,6 @@
 # Predict on all test images
 model.predict(
-    source='/content/TrafficSignProject/TrafficSignProject/images/Test',  # Path to test images folder
-    save=True,  # Save predictions
-    save_txt=True  # Save predictions as text files
+    source='/content/TrafficSignProject/images/Test',  # Path to test your images folder
+    save=True,  
+    save_txt=True  
 )
