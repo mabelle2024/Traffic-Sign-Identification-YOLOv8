462668b097749b87bf3f43881cd445e505ea5428
mabelle2024 <mabelle.george@btech.christuniversity.in>
2024-11-30 00:30:18 +0900

    test_model_singleimage.py

--- test_model_singleimage.py
+++ test_model_singleimage.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
 # Load the trained model
-model = YOLO('/content/runs/detect/train/weights/best.pt')  # Update with correct path
+model = YOLO('/content/runs/detect/train/weights/best.pt')  #replace with your correct path
 # Predict on a specific test image
-results = model.predict(source='/content/TrafficSignProject/TrafficSignProject/images/Test/00000.png', save=True)
+results = model.predict(source='/content/TrafficSignProject/TrafficSignProject/images/Test/00000.png', save=True) #replace with path of any test image
 # Access the first result in the results list
-results[0].show()  # This displays the predicted image in Colab
+results[0].show()  
